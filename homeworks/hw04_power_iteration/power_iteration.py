f681fb1a42cdbdb0d88f950337a072a3bfa82500
PozhiloyPumba <ivanov.in@phystech.edu>
2023-10-02 01:51:52 +0300

    added fourth homework

--- homeworks/hw04_power_iteration/power_iteration.py
+++ homeworks/hw04_power_iteration/power_iteration.py
@@ -11,4 +11,4 @@ def get_dominant_eigenvalue_and_eigenvector(data, num_steps):
     """
     ### YOUR CODE HERE
 
-    return 
+    return  [prev := [0, np.random.randn(len(data))]] and [prev := [float(prev[1].dot(data.dot(prev[1])) / (prev[1].dot(prev[1]))), data.dot(prev[1])/np.linalg.norm(data.dot(prev[1]))] for _ in range(1, num_steps)][-1]
